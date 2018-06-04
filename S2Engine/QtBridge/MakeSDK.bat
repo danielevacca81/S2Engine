9548b698efbf60756601abceacc78abf5e200824
d.vacca <d.vacca@idsgeoradar.com>
2018-06-04 09:33:37 +0200

    QtBridge: MakeSDK updated

--- S2Engine/QtBridge/MakeSDK.bat
+++ S2Engine/QtBridge/MakeSDK.bat
@@ -1,27 +1,26 @@
 @echo off
 cls
 
-set DESTFOLDER="..\..\SDK\S2Qt"
+set DESTFOLDER="..\..\SDK\QtBridge"
 
 rmdir /s /q %DESTFOLDER%
 mkdir %DESTFOLDER%
 
-mkdir %DESTFOLDER%\bin\Debug
-mkdir %DESTFOLDER%\bin\Release
+mkdir %DESTFOLDER%\bin\x64\Debug
+mkdir %DESTFOLDER%\bin\x64\Release
 
 mkdir %DESTFOLDER%\include
-mkdir %DESTFOLDER%\include\S2Qt
+mkdir %DESTFOLDER%\include\qtbridge
 
-copy /Y .\src\S2QtAPI.h        %DESTFOLDER%\include\
-copy /Y .\src\QtFwk\*.h        %DESTFOLDER%\include\S2Qt
+copy /Y .\src\*.h                      %DESTFOLDER%\include\qtbridge
 
-copy /Y .\bin\Debug\S2Qt.dll   %DESTFOLDER%\bin\Debug
-copy /Y .\bin\Debug\S2Qt.lib   %DESTFOLDER%\bin\Debug
-copy /Y .\bin\Debug\S2Qt.pdb   %DESTFOLDER%\bin\Debug
+copy /Y .\bin\x64\Debug\QtBridge.dll   %DESTFOLDER%\bin\x64\Debug
+copy /Y .\bin\x64\Debug\QtBridge.lib   %DESTFOLDER%\bin\x64\Debug
+copy /Y .\bin\x64\Debug\QtBridge.pdb   %DESTFOLDER%\bin\x64\Debug
 
-copy /Y .\bin\Release\S2Qt.dll %DESTFOLDER%\bin\Release
-copy /Y .\bin\Release\S2Qt.lib %DESTFOLDER%\bin\Release
-copy /Y .\bin\Release\S2Qt.pdb %DESTFOLDER%\bin\Release
+copy /Y .\bin\x64\Release\QtBridge.dll %DESTFOLDER%\bin\x64\Release
+copy /Y .\bin\x64\Release\QtBridge.lib %DESTFOLDER%\bin\x64\Release
+copy /Y .\bin\x64\Release\QtBridge.pdb %DESTFOLDER%\bin\x64\Release
 													
 echo Done!
 
